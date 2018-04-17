9be61f02ff60a06b00a6123dbf70d3291648661b
edisonxiang <xiang.edison@gmail.com>
2018-04-17 11:15:06 +0800

    add drs services

--- flexibleengine/config.go
+++ flexibleengine/config.go
@@ -454,3 +454,10 @@ func (c *Config) fwV2Client(region string) (*golangsdk.ServiceClient, error) {
 		Availability: c.getHwEndpointType(),
 	})
 }
+
+func (c *Config) drsV2Client(region string) (*golangsdk.ServiceClient, error) {
+	return huaweisdk.NewDRSServiceV2(c.HwClient, golangsdk.EndpointOpts{
+		Region:       c.determineRegion(region),
+		Availability: c.getHwEndpointType(),
+	})
+}
